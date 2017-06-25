9623e78ff01aca8b044d5b85fb9bc40d2806b8e4
kts-desilva <ktsdesilva.15@cse.mrt.ac.lk>
2017-06-25 23:41:23 +0530

    Add files via upload

---
+++ firstPHP/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        /*class world{
+            function exo(){
+                echo "We are one!";
+            }
+        }
+        $w=new world();
+        $w->exo();*/
+        echo "Fihiting EXO"
+        ?>
+    </body>
+</html>
